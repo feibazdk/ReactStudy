34f8cc3a1872a82b030a8e799dee763697b1819d
feibazdk <1577886278@qq.com>
2018-10-16 08:46:19 +0800

    dev init

---
+++ 001-webpackinit/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+// 创建插件实例对象
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: path.join(__dirname, './src/index.html'),     // 模板源文件
+    filename: 'index.html'  //  在内存中生成的首页的名称
+})
+
+
+// 向外暴露一个打包的配置对象
+module.exports = {
+    mode: 'production',    // 选项为 development(开发环境)和 production(线上环境)
+    plugins: [
+        htmlPlugin
+    ]
+}
